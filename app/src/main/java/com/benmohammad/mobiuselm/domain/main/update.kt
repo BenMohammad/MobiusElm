5f9be9ea2902c9144515c56a5d9093097a5891e5
Ben Mohammad <benmohammad593@gmail.com>
2020-08-28 16:45:58 +0100

    update.kt added...

---
+++ app/src/main/java/com/benmohammad/mobiuselm/domain/main/update.kt
@@ -0,0 +1,19 @@
+package com.benmohammad.mobiuselm.domain.main
+
+import com.spotify.mobius.Effects
+import com.spotify.mobius.Next
+import com.spotify.mobius.Update
+
+class MainUpdate: Update<MainModel, MainEvent, MainEffect> {
+
+
+    override fun update(model: MainModel, event: MainEvent): Next<MainModel, MainEffect> {
+        return when (event) {
+            is MainInit -> Next.next(
+                model, Effects.effects(LoadReposEffect)
+            )
+            is ReposLoadedEvent -> Next.next(model.copy(isLoading = false, reposList = event.reposList))
+            else -> Next.noChange()
+        }
+    }
+}
